0602c428a63d822e40a21751dfcd7d677a5e6937
hooliganspy <64080343+hooliganspy@users.noreply.github.com>
2022-06-02 02:12:58 -0400

    Creamos la data.sql

---
+++ springboot-clienteapp/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO `ciudades` (`id`, `ciudad`) VALUES
+(1, 'Luque'),
+(2, 'Asunción'),
+(3, 'Lambaré'),
+(4, 'Ciudad del Este'),
+(5, 'Encarnación');
+INSERT INTO `clientes` (`id`, `nombres`, `apellidos`, `telefono`, `email`, `ciudades_id`) VALUES
+(1, 'Diana', 'Lima', '7458-2356', 'diana@gmail.com', 1),
+(2, 'Carlos', 'Lue', '6021-8514', 'dlue@gmail.com', 3),
+(3, 'Roxana', 'López', '2451-7845', 'roxan@test.com', 5),
+(4, 'David', 'Perez', '2417-4479', 'david@gmail.com', 2);
